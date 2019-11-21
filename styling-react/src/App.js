df84ac726244f641fdba091257418ae8778f0d87
Seonyeong Jang <ssoju2827@gmail.com>
2019-11-21 17:12:20 +0900

    'eject'

---
+++ styling-react/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./SassComponent.scss";
+
+function App() {
+  return (
+    <div className="SassComponent">
+      <div className="box red" />
+      <div className="box orange" />
+      <div className="box yellow" />
+      <div className="box green" />
+      <div className="box blue" />
+      <div className="box indigo" />
+      <div className="box violet" />
+    </div>
+  );
+}
+
+export default App;

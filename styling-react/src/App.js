fb1ea4a1ec34e8a7b6710444e3d90dd3aa5ac5e0
Seonyeong Jang <ssoju2827@gmail.com>
2019-11-21 22:58:03 +0900

    1. 최적화 구현

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./SassComponent.scss";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
-    <div className="SassComponent">
-      <div className="box red" />
-      <div className="box orange" />
-      <div className="box yellow" />
-      <div className="box green" />
-      <div className="box blue" />
-      <div className="box indigo" />
-      <div className="box violet" />
-    </div>
+    <StyledComponent />
+    // <div className="SassComponent">
+    //   <div className="box red" />
+    //   <div className="box orange" />
+    //   <div className="box yellow" />
+    //   <div className="box green" />
+    //   <div className="box blue" />
+    //   <div className="box indigo" />
+    //   <div className="box violet" />
+    // </div>
   );
 }
 
---
+++ todo-app/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useCallback, useReducer } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter(todo => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      // setTodos(todos => todos.concat(todo));
+      dispatch({ type: "INSERT", todo });
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(id => {
+    // setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: "REMOVE", id });
+  }, []);
+
+  const onToggle = useCallback(id => {
+    // setTodos(todos =>
+    //   todos.map(todo =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //   ),
+    // );
+    dispatch({ type: "TOGGLE", id });
+  }, []);
+
+  return (
+    <div>
+      <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </TodoTemplate>
+    </div>
+  );
+};
+
+export default App;
